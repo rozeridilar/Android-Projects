ede4ac76bf11e5a1b09e26b5003c82a4ebc1ffdb
Rozeri Dilar <rozeri.dilar@gmail.com>
2019-05-04 16:26:29 +0300

    view_holder

--- RecyclerView/app/src/main/java/recyclerview/rozeridilar/com/recyclerview/RecyclerViewAdapter.java
+++ RecyclerView/app/src/main/java/recyclerview/rozeridilar/com/recyclerview/RecyclerViewAdapter.java
@@ -3,6 +3,7 @@ package recyclerview.rozeridilar.com.recyclerview;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
@@ -29,7 +30,9 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_listitem,viewGroup,false);
+        ViewHolder viewHolder = new ViewHolder(viewGroup);
+        return viewHolder;
     }
 
     @Override
