3df87fbcef9d6e2fb9ba8c95f0006aff3e3a45b4
Rozeri Dilar <rozeri.dilar@gmail.com>
2019-05-07 17:56:18 +0300

    fragments

--- FragmentToFragmentCommunication/app/src/main/java/rozeridilar/com/fragmenttofragmentcommunication/MainActivity.java
+++ FragmentToFragmentCommunication/app/src/main/java/rozeridilar/com/fragmenttofragmentcommunication/MainActivity.java
@@ -1,5 +1,7 @@
 package rozeridilar.com.fragmenttofragmentcommunication;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void init(){
+
+    }
+
+    private void doFragmentTransaction(Fragment fragment,String tag,boolean addToBackStack, String message){
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_container, fragment);
+        if (addToBackStack){
+            fragmentTransaction.addToBackStack(tag);
+        }
+        fragmentTransaction.commit();
+
+
     }
 }
