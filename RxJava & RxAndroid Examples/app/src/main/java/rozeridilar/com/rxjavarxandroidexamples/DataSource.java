e929af850b2dedcfbe5c52473bb77088aa393209
Rozeri Dilar <rozeri.dilar@gmail.com>
2019-05-08 14:01:17 +0300

    RxJava & RxAndroid

---
+++ RxJava & RxAndroid Examples/app/src/main/java/rozeridilar/com/rxjavarxandroidexamples/DataSource.java
@@ -0,0 +1,17 @@
+package rozeridilar.com.rxjavarxandroidexamples;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataSource {
+
+    public static List<Task> createTaskList() {
+        List<Task> tasks = new ArrayList<>();
+        tasks.add(new Task("Take out the trash", true, 3));
+        tasks.add(new Task("Walk the gumus", false, 4));
+        tasks.add(new Task("Make my bed", true, 1));
+        tasks.add(new Task("Unload the dishwasher", false, 3));
+        tasks.add(new Task("Make dinner", true, 5));
+        return tasks;
+    }
+}
--- RxJava & RxAndroid Examples/app/src/main/java/rozeridilar/com/rxjavarxandroidexamples/MainActivity.java
+++ RxJava & RxAndroid Examples/app/src/main/java/rozeridilar/com/rxjavarxandroidexamples/MainActivity.java
@@ -2,12 +2,50 @@ package rozeridilar.com.rxjavarxandroidexamples;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.graphics.drawable.DrawerArrowDrawable;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observable<Task> taskObservable = Observable
+                .fromIterable(DataSource.createTaskList())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+
+        taskObservable.subscribe(new Observer<Task>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "onSubscribe: called.");
+            }
+
+            @Override
+            public void onNext(Task task) {
+                Log.d(TAG, "onNext: " + Thread.currentThread().getName());
+                Log.d(TAG, "onNext: " + task.getDescription());
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: ", e );
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete: called.");
+            }
+        });
     }
 }
